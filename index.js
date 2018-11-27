dbd8913d74b2c76ed4e74fc06f95a38be86b8b12
Louise Foussat <louisefoussat@gmail.com>
2018-11-27 12:19:14 +0000

    Add defineStore

---
+++ index.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+
+const updateObject = (props, state) => ({ ...state, ...props })
+const neverChange = []
+
+export const defineStore = (initialValue, { get, ...rawActions } = {}) => {
+  let currentValue = initialValue
+  const subscribers = new Set()
+  const actions = {}
+
+  actions.use = () => {
+    const [value, setValue] = useState(currentValue)
+    useEffect(() => {
+      subscribers.add(setValue)
+      return () => subscribers.delete(setValue)
+    }, neverChange)
+    return value
+  }
+
+  const set = (actions.set = value => {
+    currentValue = value
+    for (const sub of subscribers) {
+      sub(value)
+    }
+  })
+
+  if (rawActions.reset === undefined) {
+    rawActions.reset = () => initialValue
+  }
+
+  if (currentValue && typeof currentValue === 'object') {
+    if (!Array.isArray(currentValue)) {
+      if (rawActions.update === undefined) {
+        rawActions.update = updateObject
+      }
+    }
+  }
+
+  for (const [key, action] of Object.entries(rawActions)) {
+    actions[key] = value => set(action(value, currentValue))
+  }
+
+  actions.get = get === undefined ? () => currentValue : () => get(currentValue)
+
+  return actions
+}
